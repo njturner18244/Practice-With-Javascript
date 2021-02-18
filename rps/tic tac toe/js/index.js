b27be4aca5c83f0e55960786d33f37b617a5030f
Nate18244 <nath182182@hotmail.com>
2021-02-18 18:56:37 +0100

    Tic tac toe
    tic tac toe, solved isses with box size changing and refresh not working. Still working on change from a nought to a cross

---
+++ rps/tic tac toe/js/index.js
@@ -0,0 +1,19 @@
+import Game from "./game.js";
+import GameView from "./GameView.js"
+
+let game = new Game();
+let gameView = new GameView(document.getElementById("app"));
+
+
+gameView.onTileClick = function (i) {
+    game.makeMove(i);
+    gameView.update(game);
+};
+
+gameView.onRestartClick = function () {
+    game = new Game();
+    gameView.update(game);
+};
+
+gameView.update(game);
+
